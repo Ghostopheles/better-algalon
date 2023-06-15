058605418c964fe12854d45140966edf9b04f25b
Ghost - Ben <Bcaltons5@gmail.com>
2023-06-15 06:12:08 -0500

    Disable the Blizzard API

--- bot.py
+++ bot.py
@@ -113,7 +113,7 @@ class CDNBotHelpCommand(commands.HelpCommand):
 class CDNBot(bridge.Bot):
     """This is the almighty CDN bot, also known as Algalon. Inherits from `discord.ext.bridge.Bot`."""
 
-    COGS_LIST = ["watcher", "api.blizzard"]
+    COGS_LIST = ["watcher"]
 
     def __init__(self, command_prefix, help_command=None, **options):
         command_prefix = command_prefix or "!"
